a48439f485d5ecd6a5420d5beebbecf2e6728b8c
ppamppamman <ryou9305@naver.com>
2024-02-12 22:05:12 +0900

    fix: wol3 proxy middleware

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ const api = require("./api");
 
 const { IPTIME_DDNS, IPTIME_TOKEN, IPTIME_MAC, PORT_FOR_ACCESS } = process.env;
 
-const httpProxy = require("http-proxy");
-const proxy = httpProxy.createProxyServer({});
-
-proxy.on("proxyReq", function (proxyReq, req, res, options) {
-  proxyReq.setHeader("Authorization", `Basic ${IPTIME_TOKEN}`);
-});
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 app.use("/api", api);
 
 // proxyReqPathResolver: function (req) {
 //       return req.url + `cgi-bin/wol_apply.cgi?act=wakeup&mac=${IPTIME_MAC}`;
 //     },
-
-app.get("/wol3/*", (req, res) => {
-  proxy.web(req, res, {
-    target: `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/`,
-    query: {
-      act: "wakeup",
-      mac: IPTIME_MAC,
-    },
-  });
+const proxy = createProxyMiddleware("/wol3", {
+  onProxyReq: function (proxyReq, req, res) {
+    proxyReq.setHeader("Authorization", `Basic ${IPTIME_TOKEN}`);
+  },
+  target: `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/`,
+  router: function (req) {
+    return `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/cgi-bin/wol_apply.cgi?act=wakeup&mac=${IPTIME_MAC}`;
+  },
 });
+
+app.get("/wol3/", proxy);
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
