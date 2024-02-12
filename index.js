db85116bb40109d3180f688bd29e0cf3fe93a936
ppamppamman <ryou9305@naver.com>
2024-02-12 22:12:47 +0900

    fix: wol3 proxy options

--- index.js
+++ index.js
@@ -10,17 +10,18 @@ app.use("/api", api);
 // proxyReqPathResolver: function (req) {
 //       return req.url + `cgi-bin/wol_apply.cgi?act=wakeup&mac=${IPTIME_MAC}`;
 //     },
-const proxy = createProxyMiddleware("/wol3", {
+const proxy = createProxyMiddleware({
+  pathFilter: "/wol3",
   onProxyReq: function (proxyReq, req, res) {
     proxyReq.setHeader("Authorization", `Basic ${IPTIME_TOKEN}`);
   },
-  target: `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/`,
-  router: function (req) {
-    return `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/cgi-bin/wol_apply.cgi?act=wakeup&mac=${IPTIME_MAC}`;
-  },
+  target: `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/cgi-bin/wol_apply.cgi?act=wakeup&mac=${IPTIME_MAC}`,
+  // router: function (req) {
+  //   return `${IPTIME_DDNS}:${PORT_FOR_ACCESS}/cgi-bin/wol_apply.cgi?act=wakeup&mac=${IPTIME_MAC}`;
+  // },
 });
 
-app.get("/wol3/", proxy);
+app.use("/wol3", proxy);
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
